499a10b8aa74510189c2d8e12928d47ce5668aa3
Justin-Stinson-Bose <justin_stinson@bose.com>
2021-05-28 13:53:08 -0500

    Add publicHeadersPath to Package.swift

--- Package.swift
+++ Package.swift
@@ -21,6 +21,7 @@ let package = Package(
         .target(
             name: "JFMinimalNotifications",
             dependencies: [],
-            path: "JFMinimalNotification"),
+            path: "JFMinimalNotification",
+            publicHeadersPath: ".")
     ]
 )
